0b5ad58408341786247dc36bf384a8086bc50b48
Jeff Cela <cela.j@labrador-company.com>
2021-12-07 00:53:47 -0500

    db bug

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const apiRoutes = require("./routes/routeAPI.js");
-const htmlRoutes = require("./routes/routeHTML.js")
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,15 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/databaseWorkout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
 
-app.use(htmlRoutes);
-app.use(apiRoutes);
+mongoose.connection.once('open', function() {
+  console.log('Connection has been made!'); 
+})
+.on('error', function(error) {
+  console.log("Connection Error:", error); 
+}); 
+
+require("./routes/apiRoutes")(app); 
+require("./routes/viewRoutes")(app); 
+
+
 app.listen(PORT, () => {
-  console.log(`Workout App listening on port ${PORT}.`);
+  console.log(`App running on port ${PORT}!`);
 });
